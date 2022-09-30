ea0339ab10482cb11d15c552d719c0255b54f042
gao <qwdra@gmail.com>
2022-09-30 17:47:33 +0800

    2022年9月30日 15点50分11

--- src/test1/test4.java
+++ src/test1/test4.java
@@ -3,20 +3,26 @@ package test1;
 public class test4 {
     public static void main(String[] args) {
 
-        //实例化一只猫（创建一只猫对象）
-        Cat cat1 = new Cat();
+
+        //实例化一只猫（创建一只猫对象） 就是实例化对象
+        Cat cat1 = new Cat();//把创建的猫的引用赋给cat1
 
         cat1.name = "🔅";
         cat1.age = 12;
 
-        System.out.println(cat1.name +" "+ cat1.age);
+        //用一个对象可以管理多个属性
 
-        System.out.println("tyhrdcrty与韩国黄金矿工预估 就有户口挂靠");
+        //创建第二个猫对象
+        Cat cat2 = new Cat();
+        cat2.name = "❀";
+        cat2.age = 3;
+        cat2.color = "white";
+
+
+        System.out.println(cat1.name +" "+ cat1.age);
 
 
     }
-
-
 }
 
 //定义一个猫类 cat  -->  自定义的数据类型
@@ -25,5 +31,7 @@ class Cat{
     String name; //猫的名字
     int age; //猫的年龄
     String color; //猫的颜色
+
+    //还可以添加行为
 
 }
